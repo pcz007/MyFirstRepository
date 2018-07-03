b341697a99a2da7ab944385156049e29af623c1e
PC <p.cz@outlook.com>
2018-07-03 17:27:12 +0200

    Ostatni commit i push

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Teraz zostały skasowane wszystkie poprzednie linie");
-        
+        System.out.println("***Koniec zadania domowego***");
+
     }
 }
