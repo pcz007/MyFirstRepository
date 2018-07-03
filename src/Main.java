90bde79e3cd8071c51e981a3ed5e16f35387678e
PC <p.cz@outlook.com>
2018-07-03 17:18:49 +0200

    Próba usunięcia poprzednich linii

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Uczę");
-        System.out.println("się");
-        System.out.println("programowania");
-        System.out.println("i");
-        System.out.println("commitowania");
-        System.out.println("Dodano dwie nowe linie");
-        System.out.println("potwierdzam dwie nowe linie");
+        System.out.println("Teraz zostały skasowane wszystkie poprzednie linie");
+        
     }
 }
